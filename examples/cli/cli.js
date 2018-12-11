3e7f03b1f31365c7b9e34e92ccef71f6752e41fd
Maik Hansen <kodekid@Maiks-MBP.fritz.box>
2018-12-11 18:56:15 +0100

    cli script and func call

--- examples/cli/cli.js
+++ examples/cli/cli.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
+/** Try cli function */
 try {
+  /** Slice first arguments */
   var args = process.argv.slice(2)
   if (args === undefined) throw new Error('No arguments provided')
 
+  /** Check if script name is set */
   var scriptName = args[0]
   if (scriptName === undefined || scriptName === '') throw new Error('No script name provided')
 
+  /** Check if action name is set */
   var actionName = args[1]
   if (actionName === undefined || actionName === '') throw new Error('No action name provided')
 
-  
+  /** Require script and call function */
+  var cliScript = require(`./src/${scriptName}.js`)
+  cliScript[actionName]()
 } catch (e) {
   console.log(e.message)
 }
