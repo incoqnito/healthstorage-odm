9a9bab28e0f53a80286295401081bdac46054dbe
Maik Hansen <kodekid@Maiks-MBP.fritz.box>
2019-04-10 09:27:20 +0200

    implement instance and model function to new model structure

--- src/core/hsModel.js
+++ src/core/hsModel.js
@@ -19,7 +19,7 @@ export default class HsModel {
 
     /**
      * Get sort asc type
-     * @return {String} ASC
+     * @return {String}
      */
     static get ASC () {
         return ASC
@@ -27,7 +27,7 @@ export default class HsModel {
 
     /**
      * Get sort desc type
-     * @return {String} DESC
+     * @return {String}
      */
     static get DESC () {
         return DESC
@@ -35,7 +35,7 @@ export default class HsModel {
 
     /**
      * Get meta id type
-     * @return {String} MD_ID
+     * @return {String}
      */
     static get MD_ID () {
         return MD_ID
@@ -43,7 +43,7 @@ export default class HsModel {
 
     /**
      * Get meta revision type
-     * @return {String} MD_REVISION
+     * @return {String}
      */
     static get MD_REVISION () {
         return MD_REVISION
@@ -51,7 +51,7 @@ export default class HsModel {
 
     /**
      * Get meta date type
-     * @return {String} MD_DATE
+     * @return {String}
      */
     static get MD_DATE () {
         return MD_DATE
@@ -80,6 +80,19 @@ export default class HsModel {
             }
         })
     }
+
+    /**
+     * Get sdo by identifier
+     * @param {String} id
+     * @returns {Promise}
+     */
+    static findById (id) {
+        return HsModel.HsAdapter.getSdo({id: id}).then(sdo => {
+            let model = new HsModel(sdo)
+            model.lockValue = lockValue !== null ? JSON.parse(lockValue) : null
+            return model
+        })
+    }
     
     /**
      * Get sdo by where
@@ -92,13 +105,35 @@ export default class HsModel {
         return this.findAll()
             .then(sdos => {
                 let matchedEntries = []
-                sdos.list.forEach(models => {
+                sdos.list.forEach(model => {
                     Object.keys(where).forEach(key => {
-                        if(models[key] === where[key]) matchedEntries.push(models)
+                        if(model[key] === where[key]) matchedEntries.push(model)
                     })
                 })
                 return (matchedEntries[0] !== undefined) ? matchedEntries[0] : false
             })
+    }
+
+    /**
+     * Get blob by identifier
+     * @param {String} id
+     * @returns {Promise}
+     */
+    static findBlobById (id) {
+        return HsModel.HsAdapter.getSdoBlob({id: id}).then(blob => {
+            return blob
+        })
+    }
+
+    /**
+     * Get blob by identifier
+     * @param {String} id
+     * @returns {Promise}
+     */
+    static findBlobFileById (id, blobId) {
+        return HsModel.HsAdapter.getSdoBlobFile({id: id, blobId: blobId}).then(blob => {
+            return blob
+        })
     }
 
     /**
@@ -107,11 +142,224 @@ export default class HsModel {
      * @returns {Promise}
      * @issue API not returns created object, workaround implemented for sdo and sdoBlob
      */
-    create (props) {
+    static create (props) {
         let propsForSdo = Object.assign(props, {md: HsModel.HsSchema.generateMd()})
         return HsModel.HsAdapter.validateSdo(propsForSdo).then(validated => {
             if (validated) {
                 return HsModel.HsAdapter.createSdo(propsForSdo).then(sdo => new HsModel(sdo))
+            }
+        })
+    }
+
+    /**
+     * Check if sdo changed since specified item
+     * @param {String} sdoId
+     * @param {String} r
+     * @returns {Promise}
+     */
+    static changedSinceByIdAndRevision (sdoId, r) {
+        return HsModel.HsAdapter.sdoHasChanged(sdoId, r).then(response => {
+            return response
+        })
+    }
+
+    /** 
+     * Update sdo by its identifier
+     * @param {Object} data
+     * @returns {HsModel} 
+     */
+    static updateById (data) {
+        data.md.r += 1
+        return HsModel.HsAdapter.validateSdo(data).then(validated => {
+            if (validated) {
+                return HsModel.HsAdapter.editSdo(data).then(sdo => new HsModel(sdo))
+            }
+        })
+    }
+
+    /**
+     * Update sdos by given where
+     * @param {Object} where 
+     * @param {Object} data 
+     */
+    static updateWhere (where, data) {
+        console.log('Update sdo by where')
+    }
+
+    /**
+     * Bulk create sdos
+     * @param {Array} bulkList
+     * @returns {Promise}
+     */
+    static bulkCreate (bulkList) {
+        console.log('Bulk create sdos')
+    }
+
+    /**
+     * Bulk update sdos
+     * @param {Array} bulkList
+     * @returns {Promise}
+     */
+    static bulkUpdate (bulkList) {
+        let collectedSods = bulkList.map(item => {
+            item.md.r += 1
+            return item._dataValues
+        })
+        return HsModel.HsAdapter.editSdosBulk(collectedSods).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Archive sdo by its identifier
+     * @returns {Promise}
+     */
+    static archiveById (id) {
+        console.log('Archive sdo by its identifier')
+    }
+
+    /**
+     * Archive sdos by given where
+     * @returns {Promise}
+     */
+    static archiveWhere (where) {
+        console.log('Archive sdos by where')
+    }
+
+    /**
+     * Lock object by its identifier
+     * @param {String} id
+     * @returns {Object}
+     */
+    static lockById (id) {
+        return HsModel.HsAdapter.lockItem(id).then(lockValue => {
+            return lockValue
+        })
+    }
+
+    /**
+     * Lock object by its identifier
+     * @param {String} id
+     * @returns {Object}
+     */
+    static unlockById (id, lockValueId) {
+        return HsModel.HsAdapter.unlockItem(id, lockValueId).then(response => {
+            return response !== undefined
+        })
+    }
+
+    /**
+     * Get lock value on sdo by its identifier and lock value
+     * @param {String} sdoId
+     * @param {String} lockValue
+     */
+    static getLockDataById (sdoId, lockValueId) {
+        return HsModel.HsAdapter.getLockData(sdoId, lockValueId).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Check if sdo is locked
+     * @param {String} sdoId
+     * @param {String} lockValue
+     */
+    static isLockedById (sdoId, lockValueId) {
+        return HsModel.HsAdapter.isLockedItem(sdoId, lockValueId).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Check if sdo exists with lock State
+     * @param {String} sdoId
+     * @param {Boolean} lockState
+     */
+    static existsInLockStateById (sdoId, lockState) {
+        return HsModel.HsAdapter.existInLockState(sdoId, lockState).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Delete sdo by its identifier
+     * @param {String} id
+     * @param {Object} data
+     */
+    static deleteById (id) {
+        return HsModel.HsAdapter.deleteSdo(id).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Delete sdo by where
+     * @param {String} id
+     * @param {Object} data
+     */
+    static deleteWhere (where) {
+       console.log("Delete sdos by where")
+    }
+
+    /**
+     * Get archive for sdo
+     * @param {String} sdoId
+     * @param {Integer} pageNo
+     * @param {Integer} pageSize
+     * @returns {Promise}
+     */
+    static getArchiveBySdoId (sdoId, pageNo = 1, pageSize = 10) {
+        return HsModel.HsAdapter.getSdoArchive(sdoId, pageNo, pageSize).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Get revisons archive for sdo
+     * @param {String} sdoId
+     * @returns {Promise}
+     */
+    static getRevisionsArchiveBySdoId (sdoId) {
+        return HsModel.HsAdapter.getSdoRevisionsArchive(sdoId).then(response => {
+            return response
+        })
+    }
+
+    /**
+     * Find meta fields
+     * @param {String} key meta field key
+     * @returns {String}
+     */
+    static findMetaField (key) {
+        var value = ''
+        switch (key) {
+            case 'id':
+                value = HsModel.MD_ID
+                break
+            case 'r':
+                value = HsModel.MD_REVISION
+                break
+            case 'tsp':
+                value = HsModel.MD_DATE
+                break
+        }
+        return value
+    }
+
+    /**
+     * Create a new sdo for given schema
+     * @param {Object} data
+     * @returns {Promise}
+     */
+    static createBlob (sdoBlob) {
+        let sdo = JSON.parse(sdoBlob.getAll('sdo'))
+        sdo['md'] = HsModel.HsSchema.generateMd()
+        return HsModel.HsAdapter.validateSdo(sdo).then(validated => {
+            if (validated) {
+                sdoBlob.set('sdo', JSON.stringify(sdo))
+                return HsModel.HsAdapter.createSdoBlob(sdoBlob).then(response => {
+                    return response
+                })
             }
         })
     }
@@ -127,6 +375,7 @@ export default class HsModel {
                 get: function () { return this._props[key] },
                 set: function (value) {
                     if (this._props[key] !== value) {
+                        this.setRevision()
                         this._props[key] = value
                     }
                 }
@@ -136,8 +385,100 @@ export default class HsModel {
 
     /**
      * Save created model with its properties
+     * @return {Promise}
      */
     save() {
-        return this.create(this._props)
+        return HsModel.create(this._props)
+    }
+
+    /**
+     * Update model with given data
+     * @return {Promise}
+     */
+    update() {
+        return HsModel.updateById(this.md.id, this._props)
+    }
+
+    /**
+     * Archive model
+     * @return {Promise}
+     */
+    archive() {
+        return HsModel.archiveById(this.md.id)
+    }
+
+    /**
+     * Get archived revison numbers for model
+     * @return {Promise}
+     */
+    getArchivedRevisions() {
+        return HsModel.getRevisionsArchiveBySdoId(this.md.id)
+    }
+
+    /**
+     * Get archived sdos mof model
+     * @param {Integer} pageNo
+     * @param {Integer} pageSize
+     * @return {Promise}
+     */
+    getArchive(pageNo = 1, pageSize = 10) {
+        return HsModel.getArchiveBySdoId(this.md.id, pageNo, pageSize)
+    }
+
+    /**
+     * Check model last change since
+     * @return {Promise}
+     */
+    changedSince() {
+        return HsModel.changedSinceByIdAndRevision(this.md.id, this.md.r)
+    }
+
+    /**
+     * Lock model
+     * @return {Promise}
+     */
+    lock() {
+        return HsModel.lockById(this.md.id)
+    }
+
+    /**
+     * Unlock model
+     * @return {Promise}
+     */
+    unlock() {
+        return HsModel.unlockById(this.md.id)
+    }
+
+    /**
+     * Get lock data for model
+     * @return {Promise}
+     */
+    getLockData() {
+        return HsModel.getLockDataById(this.md.id, this.lockValue.value)
+    }
+
+    /**
+     * Check if model is locked
+     * @return {Promise}
+     */
+    isLocked() {
+        return HsModel.isLockedById(this.md.id)
+    }
+
+    /**
+     * Check if model exists in specific lock state
+     * @param {String} lockState
+     * @return {Promise}
+     */
+    isInLockState(lockState) {
+        return HsModel.existsInLockStateById(this.md.id, lockState)
+    }
+
+    /**
+     * Store revision to local model
+     */
+    setRevision () {
+        if (this.revision === undefined) this.revision = {}
+        this.revision[new Date().getTime()] = Object.assign({}, this._props)
     }
 }
